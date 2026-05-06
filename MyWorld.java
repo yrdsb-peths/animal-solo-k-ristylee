5ed8503803e0a1376f443a1ca57c441cacb1a2cd
k-ristylee <kristy.lee12280@gmail.com>
2026-05-06 11:52:39 -0400

    make apple and elephant

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,25 @@
 import greenfoot.*;
 
-public class MyWorld extends World {
-    public MyWorld() {
+/**
+ * The World our hero lives in.
+ * 
+ * @author Kristy
+ * @version May 2026
+ */
+public class MyWorld extends World
+{
+    /**
+     * Constructor for object of class MyWorld 
+     */
+    public MyWorld()
+    {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
+        
     }
 }
