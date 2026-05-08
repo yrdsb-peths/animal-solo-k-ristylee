6e93dcdc9ffcc538ad3ead3fc390a445a040608b
k-ristylee <kristy.lee12280@gmail.com>
2026-05-08 08:51:06 -0400

    3.2

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("Hungry Elephant", 55);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,7 +18,7 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
 
-        addObject(titleLabel, getWidth()/2, 200);
+        addObject(titleLabel, 230, 65);
         prepare();
     }
 
@@ -42,7 +42,7 @@ public class TitleScreen extends World
     private void prepare()
     {
         Elephant elephant = new Elephant();
-        addObject(elephant,450,82);
+        addObject(elephant, 495, 82);
     }
 }
 
